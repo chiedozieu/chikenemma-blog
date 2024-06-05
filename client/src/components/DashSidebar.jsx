a3a5a322eee0f95fa677e3ab87df845c23773188
chiedozieu <chiedozieugwor@gmail.com>
2024-06-05 15:20:47 +0100

    Profile page ui

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -16,11 +16,11 @@ export default function DashSidebar() {
      }, [location.search]);
      
   return (
-    <Sidebar className="w-full md:w-56 "> 
+    <Sidebar className="w-full md:w-56"> 
         <Sidebar.Items>
            <Sidebar.ItemGroup>
-                <Link to='/dashboard?tab=profile'>
-                    <Sidebar.Item active={tab==='profile'} icon={HiUser} label={'User'} labelColor='dark'   >
+                <Link to='/dashboard?tab=profile' >
+                    <Sidebar.Item active={tab==='profile'} icon={HiUser} label={'User'} labelColor='dark' as='div' >
                         Profile
                     </Sidebar.Item>
                 </Link>
