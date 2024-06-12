0cbc373d66d233a77e1a3ec4a3e48e38bc3d77cf
chiedozieu <chiedozieugwor@gmail.com>
2024-06-12 14:34:25 +0100

    create post ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -8,11 +8,12 @@ import 'react-circular-progressbar/dist/styles.css';
 import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { MdAutoDelete } from "react-icons/md";
+import { Link } from 'react-router-dom';
 
 
 
 export default function DashProfile() {
-    const { currentUser, error } = useSelector((state) => state.user)
+    const { currentUser, error, loading } = useSelector((state) => state.user)
     const [ imageFile, setImageFile ] = useState(null)
     const [ imageFileUrl, setImageFileUrl ] = useState(null)
     const [ imageFileUploadingProgress, setImageFileUploadingProgress  ] = useState(null)
@@ -212,10 +213,28 @@ const handleSignout = async () => {
              onChange={handleChange} 
              />
              
-             <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-                Update
+             <Button 
+             type='submit' 
+             gradientDuoTone='purpleToBlue' 
+             outline 
+             disabled={loading || imageFileUploading}>
+             {loading? 'loading...' : 'Update'}
              </Button>
+             {
+                currentUser.isAdmin && (
+                    <Link to='/create-post'>
+                        <Button type='button' gradientDuoTone='purpleToPink'
+                        className='w-full '
+                        >
+                        Create Post
+                        </Button>
+                    </Link> 
+                )
+             }
         </form> 
+
+
+
         <div className="text-red-500 flex justify-between mt-5">
             <span onClick={()=>setShowDeleteModal(true)} className='cursor-pointer'>Delete Account</span>
             <span onClick={handleSignout} className='cursor-pointer'>Sign Out</span>
